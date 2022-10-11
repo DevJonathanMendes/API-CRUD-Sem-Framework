3f6f6ce99a7eaf691ff5aa5780c4a4447a2e308a
DevJonathanMendes <jonathanbarreiro88@gmail.com>
2022-10-11 12:12:42 -0300

    Rotas importadas.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const http = require("http");
+const routes = require("./routes/index");
 
-const server = http.createServer();
+const server = http.createServer(routes);
 server.listen(process.env.PORT || 3000);
