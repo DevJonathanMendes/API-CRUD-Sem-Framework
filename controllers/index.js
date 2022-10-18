3b167e54fee3a6c0e1a6d7cd3f0eff0503c5eebf
DevJonathanMendes <jonathanbarreiro88@gmail.com>
2022-10-18 17:49:16 -0300

    Método GetAllBooks Adicionado.

--- controllers/index.js
+++ controllers/index.js
@@ -1,4 +1,14 @@
+const { readFile, writeFile } = require("fs");
+
+const pathFile = __dirname + "/../data/books.json";
+const encoding = "utf8";
+
 const controllers = {
+    getAllBooks: () => {
+        readFile(pathFile, encoding, (error, fileBuffer) => {
+
+        });
+    },
 };
 
 module.exports = controllers;
