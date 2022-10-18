4a01641833fb60a699d1cda0aa23c021c3eb7d28
DevJonathanMendes <jonathanbarreiro88@gmail.com>
2022-10-18 18:53:24 -0300

    Response Importado.

--- controllers/index.js
+++ controllers/index.js
@@ -1,4 +1,5 @@
 const { readFile, writeFile } = require("fs");
+const { response } = require("../utils/index");
 
 const pathFile = __dirname + "/../data/books.json";
 const encoding = "utf8";
@@ -6,7 +7,7 @@ const encoding = "utf8";
 const controllers = {
     getAllBooks: () => {
         readFile(pathFile, encoding, (error, fileBuffer) => {
-
+            error ? response(500) : response(200, fileBuffer);
         });
     },
 };
