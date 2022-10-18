e25e92e61f2843f1f86d12a77a90ca4fedb5b884
DevJonathanMendes <jonathanbarreiro88@gmail.com>
2022-10-18 17:58:24 -0300

    Importado SetHttp.

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const http = require("http");
+const { SetHttp } = require("./utils/index");
 const routes = require("./routes/index");
 
-const server = http.createServer(routes);
+const server = http.createServer((req, res) => {
+    SetHttp(req, res);
+    routes(req, res);
+});
+
 server.listen(process.env.PORT || 3000);
