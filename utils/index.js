78f4bef06c34d816dbd2b5542b13492d57108586
DevJonathanMendes <jonathanbarreiro88@gmail.com>
2022-10-18 18:50:57 -0300

    Criado Método Response.

--- utils/index.js
+++ utils/index.js
@@ -3,6 +3,21 @@ const utils = {
         this.req = req;
         this.res = res;
     },
+
+    response: (code, data) => {
+        const send = (code, header, message) =>
+            res.writeHead(code, header).end(message);
+
+        switch (code) {
+            case 200: send(code, successHeader, data || "Ok."); break;
+            case 201: send(code, successHeader, "Created."); break;
+            case 204: send(code, errorHeader, "No Content."); break;
+            case 400: send(code, errorHeader, data || "Bad Request."); break;
+            case 404: send(code, errorHeader, "Not Found."); break;
+            case 501: send(code, errorHeader, "Not Implemented."); break;
+            default: send(500, errorHeader, "Internal Server Error."); break;
+        };
+    }
 };
 
 module.exports = utils;
