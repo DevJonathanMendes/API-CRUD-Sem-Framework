aa68fcacaa2ceb0b0974836b3c5bb5065ddbd357
DevJonathanMendes <jonathanbarreiro88@gmail.com>
2022-10-19 15:50:12 -0300

    Implementado Método para Deletar Livro.

--- controllers/index.js
+++ controllers/index.js
@@ -3,6 +3,9 @@ const { response } = require("../utils/index");
 
 const pathFile = __dirname + "/../data/books.json";
 const encoding = "utf8";
+const regUrlId = /\d{1,4}$/;
+
+const stringify = books => JSON.stringify(books, null, 4);
 
 const controllers = {
     getAllBooks: () => {
@@ -10,6 +13,29 @@ const controllers = {
             error ? response(500) : response(200, fileBuffer);
         });
     },
+
+    deleteBook: () => {
+        readFile(pathFile, encoding, (error, fileBuffer) => {
+            if (error) {
+                response(500);
+            } else {
+                const fileBooks = JSON.parse(fileBuffer);
+
+                const id = req.url.match(regUrlId)[0];
+                const bookIds = fileBooks.map(book => book.id);
+
+                if (bookIds.includes(Number(id))) {
+                    const books = fileBooks.filter(books => books.id != id);
+
+                    writeFile(pathFile, stringify(books), error => {
+                        error ? response(500) : response(200);
+                    });
+                } else {
+                    response(204);
+                };
+            };
+        });
+    }
 };
 
 module.exports = controllers;
